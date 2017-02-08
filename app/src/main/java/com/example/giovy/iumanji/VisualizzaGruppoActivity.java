4906bf8fcd3356f2614811f07a822ddb82c49513
alessandrotola <gul4s@yahoo.it>
2017-02-08 15:55:16 +0100

    Ale

--- app/src/main/java/com/example/giovy/iumanji/VisualizzaGruppoActivity.java
+++ app/src/main/java/com/example/giovy/iumanji/VisualizzaGruppoActivity.java
@@ -1,13 +1,62 @@
 package com.example.giovy.iumanji;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class VisualizzaGruppoActivity extends AppCompatActivity {
-
+    ImageButton vaiGruppo;
+    ImageButton vaiLocali;
+    Button visualizzaSondaggio;
+    Button creaSondaggio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizza_gruppo);
+        vaiLocali = (ImageButton) findViewById(R.id.vai_locali_gruppo);
+        vaiLocali.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View view) {
+                                             Intent showGruppo = new Intent(VisualizzaGruppoActivity.this, Locali.class);
+                                             startActivity(showGruppo);
+                                         }
+                                     }
+
+        );
+        vaiGruppo = (ImageButton) findViewById(R.id.vai_membri_button);
+        vaiGruppo.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View view) {
+                                             Intent showGruppo = new Intent(VisualizzaGruppoActivity.this, MembriGruppoActivity.class);
+                                             startActivity(showGruppo);
+                                         }
+                                     }
+
+        );
+
+        visualizzaSondaggio = (Button) findViewById(R.id.visualizza_sondaggio_button);
+        visualizzaSondaggio.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View view) {
+                                             Intent showGruppo = new Intent(VisualizzaGruppoActivity.this, VisualizzaSondaggioActivity.class);
+                                             startActivity(showGruppo);
+                                         }
+                                     }
+
+        );
+
+        creaSondaggio = (Button) findViewById(R.id.crea_sondaggio_button);
+        creaSondaggio.setOnClickListener(new View.OnClickListener() {
+                                                   @Override
+                                                   public void onClick(View view) {
+                                                       Intent showGruppo = new Intent(VisualizzaGruppoActivity.this, CreaSondaggioActivity.class);
+                                                       startActivity(showGruppo);
+                                                   }
+                                               }
+
+        );
     }
 }
