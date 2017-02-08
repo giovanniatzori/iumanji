a9fefb70b97b17e39a5f6b69626e52670ff5d4c1
paperelle <iumanji>
2017-02-08 15:44:56 +0100

    ora forse ci sono tutte

---
+++ app/src/main/java/com/example/giovy/iumanji/MembriGruppoActivity.java
@@ -0,0 +1,13 @@
+package com.example.giovy.iumanji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MembriGruppoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_membri_gruppo);
+    }
+}
--- app/src/main/java/com/example/giovy/iumanji/VisualizzaGruppoActivity.java
+++ app/src/main/java/com/example/giovy/iumanji/VisualizzaGruppoActivity.java
@@ -1,13 +1,27 @@
 package com.example.giovy.iumanji;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class VisualizzaGruppoActivity extends AppCompatActivity {
-
+    ImageButton vauGruppo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualizza_gruppo);
+
+        ImageButton vaiGruppo = (ImageButton) findViewById(R.id.vai_membri_button);
+        vaiGruppo.setOnClickListener(new View.OnClickListener() {
+                                         @Override
+                                         public void onClick(View view) {
+                                             Intent showGruppo = new Intent(VisualizzaGruppoActivity.this, MembriGruppoActivity.class);
+                                             startActivity(showGruppo);
+                                         }
+                                     }
+
+        );
     }
 }
