f83d8bbe23bd1f719eec252f581bcef49bbf644c
paperelle <iumanji>
2017-02-08 11:45:00 +0100

    aggiunto anche activity risultato sondaggio wiiii

---
+++ app/src/main/java/com/example/giovy/iumanji/RisultatoSondaggioActivity.java
@@ -0,0 +1,13 @@
+package com.example.giovy.iumanji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RisultatoSondaggioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_risultato_sondaggio);
+    }
+}
