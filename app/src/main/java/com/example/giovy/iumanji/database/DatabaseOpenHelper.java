8d96a88e2539b1d09beef42903360cd52b141f81
alessandrotola <gul4s@yahoo.it>
2017-02-12 17:13:34 +0100

    Ale

--- app/src/main/java/com/example/giovy/iumanji/database/DatabaseOpenHelper.java
+++ app/src/main/java/com/example/giovy/iumanji/database/DatabaseOpenHelper.java
@@ -9,7 +9,7 @@ import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
  */
 
 public class DatabaseOpenHelper extends SQLiteAssetHelper {
-    private static final String DATABASE_NAME = "iumangiDb.db";
+    private static final String DATABASE_NAME = "iumangiDb1.db";
     private static final int DATABASE_VERSION = 8;
 
     public DatabaseOpenHelper(Context context) {
