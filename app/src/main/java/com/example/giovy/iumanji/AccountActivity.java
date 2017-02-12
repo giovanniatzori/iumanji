1406bf383840d46253890887c094328fb423483f
paperelle <iumanji>
2017-02-12 20:31:24 +0100

    aggiunta una cardview in account

--- app/src/main/java/com/example/giovy/iumanji/AccountActivity.java
+++ app/src/main/java/com/example/giovy/iumanji/AccountActivity.java
@@ -1,9 +1,24 @@
 package com.example.giovy.iumanji;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.ImageView;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 import com.example.giovy.iumanji.database.Persona;
 
@@ -11,31 +26,39 @@ import java.io.Serializable;
 
 public class AccountActivity extends AppCompatActivity {
 
-    Persona person;
+    private RecyclerView recyclerView;
+    private AccountAdapter adapter;
+    private List<Persona> personaList;
 
-    TextView nameText, surNameText, emailText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
 
-        nameText = (TextView) findViewById(R.id.attrNomeRes);
-        surNameText = (TextView) findViewById(R.id.attrCognomeRes);
-        emailText = (TextView) findViewById(R.id.attrEmailRes);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-        Intent intent = getIntent();
-        Serializable obj =  intent.getSerializableExtra(
-                RegistrazioneActivity.PERSON_EXTRA);
+        personaList = new ArrayList<>();
+        adapter = new AccountAdapter(this, personaList);
 
-        if(obj instanceof Persona) {
-            person = (Persona) obj;
-        }
-        else{
-            person = new Persona();
-        }
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this,1);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
 
-        nameText.setText(person.getNome());
-        surNameText.setText(person.getCognome());
-        emailText.setText(person.getEmail());
+        preparePersona();
     }
+
+    private void preparePersona() {
+
+        Persona a = new Persona("Giovanni", "Atzori", "gio83.atzo@libero.it","12345");
+        personaList.add(a);
+
+        a = new Persona("Martina","Senis","mart@gmail.com","54321");
+        personaList.add(a);
+
+        adapter.notifyDataSetChanged();
+    }
+
+
 }
