3505c58cbf3ec7585665d7e42f991d5dea55864c
alessandrotola <gul4s@yahoo.it>
2017-02-12 17:16:47 +0100

    Ale

--- app/src/main/java/com/example/giovy/iumanji/database/DatabaseHelper.java
+++ app/src/main/java/com/example/giovy/iumanji/database/DatabaseHelper.java
@@ -1,8 +1,6 @@
 package com.example.giovy.iumanji.database;
 
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
 
