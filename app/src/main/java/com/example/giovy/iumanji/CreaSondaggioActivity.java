dda846af971e65386c0a3557336a28d6cd621b72
paperelle <iumanji>
2017-02-15 16:32:55 +0100

    aggiunta una cardview in account

--- app/src/main/java/com/example/giovy/iumanji/CreaSondaggioActivity.java
+++ app/src/main/java/com/example/giovy/iumanji/CreaSondaggioActivity.java
@@ -2,16 +2,15 @@ package com.example.giovy.iumanji;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.support.v7.app.AppCompatActivity;
-
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +21,10 @@ import com.example.giovy.iumanji.database.Locale;
 
 public class CreaSondaggioActivity extends AppCompatActivity {
     Button crea_sondaggio;
-    private RecyclerView recyclerView;
-    private CreaSondaggioAdapter adapter;
     private DbAdapter helper;
     private Cursor cursor;
-    private List<Locale> localeList = new ArrayList<>();
-    private EditText timerInput;
-    private long timer;
-
+    private final List<Locale> localeList = new ArrayList<>();
+    private ListView listview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,38 +33,42 @@ public class CreaSondaggioActivity extends AppCompatActivity {
         Bundle idGruppo = getIntent().getExtras();
         Integer id = idGruppo.getInt("idGruppo");
 
-        timerInput = (EditText) findViewById(R.id.cronometro_crea_sondaggio);
-
         crea_sondaggio = (Button) findViewById(R.id.crea_sondaggio_button) ;
         crea_sondaggio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent showMainMenu = new Intent(CreaSondaggioActivity.this, VisualizzaGruppoActivity.class);
-                timer = Long.parseLong(timerInput.getText().toString());
-                showMainMenu.putExtra("timerValue", timer);
+                Intent showMainMenu = new Intent(CreaSondaggioActivity.this, MainMenu.class);
                 startActivity(showMainMenu);
 
             }
         });
 
+        listview = (ListView) findViewById(R.id.lista_locali_crea_sondaggio);
+
         helper = DbAdapter.getInstance(this);
         helper.open();
-        System.out.println(id.toString() +"qui");
+
         cursor=helper.fetchGroupLocalsByFilter(id.toString());
+        //cursor=helper.fetchAllLocals();
 
 
+        final List<String> listaNomi = new ArrayList<>();
         while (cursor.moveToNext()) {
             Locale l = new Locale(cursor.getString(0), cursor.getString(1));
+
             localeList.add(l);
-            System.out.println(cursor.getString(0)+ " " +cursor.getString(1));
+            listaNomi.add(l.getNome());
+            }
+
+
+        String[] listContent = listaNomi.toArray(new String[0]);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_single_choice,
+                listContent);
+
+        listview.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        listview.setAdapter(adapter);
+
         }
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view_crea_sondaggio);
 
-        adapter = new CreaSondaggioAdapter(this, localeList);
-
-        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this,1);
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(adapter);
-    }
 }
