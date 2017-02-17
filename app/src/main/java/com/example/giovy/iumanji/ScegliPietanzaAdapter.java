e272a39e3b8c70f8395e3b602d126e3a59da2466
alessandrotola <gul4s@yahoo.it>
2017-02-17 12:11:02 +0100

    Ale

--- app/src/main/java/com/example/giovy/iumanji/ScegliPietanzaAdapter.java
+++ app/src/main/java/com/example/giovy/iumanji/ScegliPietanzaAdapter.java
@@ -6,9 +6,12 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
+
 
 import com.example.giovy.iumanji.database.Pietanza;
 
@@ -20,40 +23,99 @@ import java.util.List;
 
 public class ScegliPietanzaAdapter extends BaseAdapter {
 
-    private List<Pietanza> pietanze=null;
-    private Context context=null;
+    private List<Pietanza> pietanze;
+    LayoutInflater inflater;
+    Context context;
+    int listPosititon = 1;
 
     public ScegliPietanzaAdapter(Context context,List<Pietanza> pietanze)
     {
         this.pietanze=pietanze;
         this.context=context;
+        this.inflater = LayoutInflater.from(context);
     }
 
     @Override
-    public int getCount()
-    {
-        return pietanze.size();
+    public int getCount() {
+        return 0;
     }
 
     @Override
-    public Object getItem(int position)
-    {
-        return pietanze.get(position);
+    public Object getItem(int position) {
+        return null;
     }
 
     @Override
-    public long getItemId(int position)
-    {
-        return getItem(position).hashCode();
+    public long getItemId(int position) {
+        return 0;
     }
 
+    private class ViewHolder {
+        TextView prezzo;
+        TextView nome;
+        EditText quantita;
+    }
     @Override
-    public View getView(int position, View v, ViewGroup vg)
+    public View getView(int position, View convertView, ViewGroup parent)
     {
-        if (v==null)
-        {
-            v= LayoutInflater.from(context).inflate(R.layout.activity_scegli_pietanza_adapter, null);
+        final ViewHolder holder;
+
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = this.inflater.inflate(
+                    R.layout.activity_scegli_pietanza_adapter, parent, false);
+
+            holder.prezzo = (TextView) convertView.findViewById(R.id.prezzo);
+            holder.nome = (TextView) convertView.findViewById(R.id.NomePietanzaScelta);
+            holder.quantita = (EditText) convertView.findViewById(R.id.quantita);
+
+
+            convertView.setTag(holder);
+            convertView.setTag(R.id.prezzo, holder.prezzo);
+            convertView.setTag(R.id.NomePietanzaScelta, holder.nome);
+            convertView.setTag(R.id.quantita, holder.quantita);
+
+        } else {
+            holder = (ViewHolder) convertView.getTag();
         }
+
+        holder.quantita.setTag(position);
+
+        holder.quantita.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before,
+                                      int count) {
+                int pos = (Integer) holder.quantita.getTag();
+
+                pietanze.get(pos).setQuantita(Integer.parseInt(s.toString()));
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        holder.prezzo.setText(pietanze.get(position).getPrezzo().toString());
+        holder.nome.setText(pietanze.get(position).getNome());
+        holder.quantita.setText(pietanze.get(position)
+                .getQuantita());
+
+        return convertView;
+    }
+
+        /*if (v==null)
+    {
+        v= LayoutInflater.from(context).inflate(R.layout.activity_scegli_pietanza_adapter, null);
+    }
         final Pietanza ai=(Pietanza) getItem(position);
         TextView txt=(TextView) v.findViewById(R.id.prezzo);
         txt.setText(ai.getPrezzo().toString());
@@ -70,7 +132,7 @@ public class ScegliPietanzaAdapter extends BaseAdapter {
             public void beforeTextChanged(CharSequence s, int start, int count, int after){}
             public void onTextChanged(CharSequence s, int start, int before, int count){}
         });
-        return v;
+        return v;*/
     }
 
-}
+
