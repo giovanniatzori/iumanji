3b9b3afa6dec1a97d7b5ea01d2710bcbbf328a27
paperelle <iumanji>
2017-02-18 23:38:36 +0100

    aggiunta una cardview in account

--- app/src/main/java/com/example/giovy/iumanji/AggiungiLocale.java
+++ app/src/main/java/com/example/giovy/iumanji/AggiungiLocale.java
@@ -1,7 +1,11 @@
 package com.example.giovy.iumanji;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,11 +14,14 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.File;
+
 import static android.R.attr.data;
 
 public class AggiungiLocale extends AppCompatActivity {
 
-    private static final int PHOTO_REQUEST_CODE=0 ;
+    //private static final int PHOTO_REQUEST_CODE=0 ;
+    private static final int SELECT_IMAGE_CODE = 0;
     Button aggiungi_locale_button;
     ImageButton aggiungi_foto_button;
     ImageView photo;
@@ -36,7 +43,7 @@ public class AggiungiLocale extends AppCompatActivity {
         });
 
         aggiungi_foto_button = (ImageButton) findViewById(R.id.immagine_locale_button);
-        aggiungi_foto_button.setOnClickListener(new View.OnClickListener(){
+       /* aggiungi_foto_button.setOnClickListener(new View.OnClickListener(){
             @Override
                     public void onClick(View view){
                 Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -44,12 +51,28 @@ public class AggiungiLocale extends AppCompatActivity {
             }
         }
 
-        );
+        );*/
+
+        aggiungi_foto_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                // check the value state is null or not
+                Show_Toast("Scegliere la foto");
+                Intent in = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(in, SELECT_IMAGE_CODE);
+
+            }
+
+            private void Show_Toast(String s) {
+            }
+        });
 
 
     }
 
-    @Override
+    /*@Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
         super.onActivityResult(requestCode, resultCode, data);
@@ -57,6 +80,29 @@ public class AggiungiLocale extends AppCompatActivity {
         {
             Bitmap bp = (Bitmap) data.getExtras().get("data");
             photo.setImageBitmap(bp);
+        }
+    }*/
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SELECT_IMAGE_CODE && resultCode == Activity.RESULT_OK) {
+            Uri contentUri = data.getData();
+
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor cursor = managedQuery(contentUri,
+                    proj,  // Which columns to return
+                    null,  // WHERE clause; which rows to return (all rows)
+                    null,  // WHERE clause selection arguments (none)
+                    null); // Order-by clause (ascending by name)
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            String filePath = cursor.getString(column_index);
+            File imgFile = new File(filePath);
+            if (imgFile.exists()) {
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                //Drawable d = new BitmapDrawable(getResources(), myBitmap);
+                photo.setImageBitmap(myBitmap);
+            }
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 }
